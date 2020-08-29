1f96f5a30e3df005b5ef167bb352a12fefe990b8
RealOrge <george.top@windowslive.com>
2020-08-29 20:31:22 +0300

    Clean up the messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,17 @@ import exercises.SimpleAlgorithms;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("First Chapter");
         SimpleAlgorithms set1 = new SimpleAlgorithms();
         set1.runAllFunctions();
 
+        System.out.println();
+        System.out.println("Second Chapter");
         ArraysCollections set2 = new ArraysCollections();
         set2.runAllFunctions();
 
+        System.out.println();
+        System.out.println("Third Chapter");
         PasswordCheck set3 = new PasswordCheck("Lalilulelo1.");
         set3.testPassword();
 
