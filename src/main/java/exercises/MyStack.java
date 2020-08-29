c4b8fe5d3925ce3154645926c10d7e808f1f61b4
RealOrge <george.top@windowslive.com>
2020-08-29 19:27:52 +0300

    Add comments

--- src/main/java/exercises/MyStack.java
+++ src/main/java/exercises/MyStack.java
@@ -9,20 +9,35 @@ import java.util.List;
 public class MyStack {
     private List<Character> array;
 
+    /**
+     * Constructor for the class, initialization of data
+     */
     public MyStack(){
         array = new ArrayList<>();
     }
 
+    /**
+     * Add a character to the data
+     * @param s the character to be added
+     */
     public void push(Character s){
         array.add(s);
     }
 
+    /**
+     * Add a string to the data
+     * @param string the string to be added
+     */
     public void pushString(String string){
         for (char s: string.toCharArray()) {
             array.add(s);
         }
     }
 
+    /**
+     * Remove and return the last character from the data
+     * @return the last character of the data
+     */
     public Character pop(){
         int size = array.size();
         if (size < 1){
@@ -33,6 +48,10 @@ public class MyStack {
         return c;
     }
 
+    /**
+     * Return the last character
+     * @return the last character of the data
+     */
     public Character peek(){
         int size = array.size();
         if (size < 1){
